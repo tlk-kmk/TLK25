53d2946d79c17adf1651f49391f11a5fa876ec88
Timo Leon Krause <tlk@MacBook-Pro-von-Timo.local>
2025-05-13 15:50:42 +0200

    h

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -67,7 +67,7 @@ export default function Loader({ onComplete }: { onComplete: () => void }) {
             }}
           >
             {/* Logo */}
-            <Image src="/logo.svg" alt="Logo" width={84} height={56} />
+            <Image src="/logo.svg" alt="Logo" width={21} height={14} />
 
             {/* Progress percentage */}
             <div
